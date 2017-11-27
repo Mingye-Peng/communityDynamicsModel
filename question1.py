a626b983c7f6db950231637bb8a56bc2f9f04f40
Brittni Bertolet <bbertole@nd.edu>
2017-11-27 11:07:20 -0500

    first model

--- question1.py
+++ question1.py
@@ -1,3 +1,15 @@
-#### Biocomputing modeling project
+#### Biocomputing Modeling Project - Team Alpha Wolf
+#### Authors: Brittni Bertolet, Josh Hobgood, Aaron Long, Om Neelay
+#### Date: 11/27/2017
+
+### Load packages
+import pandas
+import scipy
+import scipy.integrate as spint
+from plotnine import *
 
 ### Question 1
+# Define custom function
+# Set times, initial values, and parameters
+# Run simulation
+
